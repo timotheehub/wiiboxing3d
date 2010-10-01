f7e4db57180b049ec536a30c2566c5c7bf68fc85
timothee.ledure <timothee.ledure@33e42587-8ed1-a3a5-a332-d5786b2c7ef2>
2010-10-01 11:43:41 +0000

    VC# Project

---
+++ trunk/WiiBoxing/GamePlay.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace WiiBoxing3D
+{
+    public class GamePlay : GameScreen
+    {
+        // Add here punching bags variables
+        // vector of punching bags
+        // 2 gloves
+
+
+        public override void Update(GameTime gameTime) { }
+        public override void Draw(GameTime gameTime) { }
+        public void CheckCollision() { }
+    }
+}
