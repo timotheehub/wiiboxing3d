c0050270e105af45166353007c160c4ffb0986a2
timothee.ledure <timothee.ledure@33e42587-8ed1-a3a5-a332-d5786b2c7ef2>
2010-10-01 11:43:41 +0000

    VC# Project

---
+++ WiiBoxing/GameObject.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework;
+
+namespace WiiBoxing3D
+{
+    /// <summary>
+    /// This is the main type for a game object.
+    /// </summary>
+    public abstract class gameObject
+    {
+        public Model model = null;
+        public Vector3 position = Vector3.Zero;
+        public Vector3 rotation = Vector3.Zero;
+        public float scale = 1.0f;
+
+        public virtual void Update() { }
+    }
+}
