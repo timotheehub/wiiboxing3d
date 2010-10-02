e6fa958929a547a27932c0d90b87a2f2117e86d9
timothee.ledure <timothee.ledure@33e42587-8ed1-a3a5-a332-d5786b2c7ef2>
2010-10-02 09:17:11 +0000

    First text and first 3D object

--- trunk/WiiBoxing/GamePlay.cs
+++ trunk/WiiBoxing/GamePlay.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace WiiBoxing3D
 {
@@ -11,22 +14,27 @@ namespace WiiBoxing3D
     /// </summary>
     public class GamePlay : GameScreen
     {
+        // Game
+        Game1 game1;
+
         // Camera variables
-        Vector3 cameraPosition = new Vector3(0.0f, 60.0f, 160.0f);
-        Vector3 cameraLookAt = new Vector3(0.0f, 50.0f, 0.0f);
+        Vector3 cameraPosition = new Vector3(0.0f, -50.0f, 20.0f);
+        Vector3 cameraLookAt = new Vector3(0.0f, 0.0f, 0.0f);
         Matrix cameraProjectionMatrix;
         Matrix cameraViewMatrix;
 
         // Object variables
+        GameObject aRandomGameObject;
         List<GameObject> gameObjects;
+
 
 
         /// <summary>
         /// Constructor
         /// </summary>
-        public GamePlay(GraphicsDeviceManager graphics)
+        public GamePlay(Game1 aGame1)
         {
-            LoadContent(graphics);
+            game1 = aGame1;
         }
 
 
@@ -34,14 +42,23 @@ namespace WiiBoxing3D
         /// <summary>
         /// Load content for the gameplay
         /// </summary>
-        public void LoadContent(GraphicsDeviceManager graphics)
+        public void LoadContent()
         {
+            // Camera
             cameraViewMatrix = Matrix.CreateLookAt(
                 cameraPosition, cameraLookAt, Vector3.Up);
 
             cameraProjectionMatrix = Matrix.CreatePerspectiveFieldOfView(
-                MathHelper.ToRadians(45.0f), graphics.GraphicsDevice.Viewport.AspectRatio,
+                MathHelper.ToRadians(45.0f), game1.graphics.GraphicsDevice.Viewport.AspectRatio,
                 1.0f, 10000.0f);
+
+            // Objects
+            gameObjects = new List<GameObject>();
+            aRandomGameObject = new GameObject();
+            aRandomGameObject.model = game1.Content.Load<Model>("Models\\Box");
+            aRandomGameObject.rotation = new Vector3(1.57f, 0, 0f);
+            aRandomGameObject.scale = 0.2f;
+            gameObjects.Add(aRandomGameObject);
         }
 
 
@@ -50,6 +67,24 @@ namespace WiiBoxing3D
         /// </summary>
         public override void Update(GameTime gameTime)
         {
+            // Keyboard test
+            KeyboardState keyboardState = Keyboard.GetState();
+            if (keyboardState.IsKeyDown(Keys.Left))
+            {
+                aRandomGameObject.rotation.Y += 0.05f;
+            }
+            if (keyboardState.IsKeyDown(Keys.Right))
+            {
+                aRandomGameObject.rotation.Y -= 0.05f;
+            }
+            if (keyboardState.IsKeyDown(Keys.Up))
+            {
+                aRandomGameObject.rotation.X += 0.05f;
+            }
+            if (keyboardState.IsKeyDown(Keys.Down))
+            {
+                aRandomGameObject.rotation.X -= 0.05f;
+            }
         }
 
 
@@ -59,6 +94,12 @@ namespace WiiBoxing3D
         /// </summary>
         public override void Draw(GameTime gameTime)
         {
+            game1.DrawText(new Vector2(150, 10), "This is how you can draw some text.", Color.Black);
+
+            foreach (GameObject gameObject in gameObjects)
+            {
+                gameObject.Draw(cameraProjectionMatrix, cameraViewMatrix);
+            }
         }
 
 
